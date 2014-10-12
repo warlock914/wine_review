a51b66dcd44cfe2da1db60e0494fac9e4954bab6
Alan_Garcia <warlock914@gmail.com>
2014-10-11 23:54:55 -0400

    Homework2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+Wine.create(name:'Louis Jadot', year:1981, winery:'Bordeaux', country:'France', varietal:'Pinot Noir')
+Wine.create(name:'Botdga Norton', year:1984, winery:'Buenos Aires', country:'Argentina', varietal:'Chardonnay')
+Wine.create(name:"Beringer", year:1986, winery:'California', country:'United States', varietal:'Cabernet Sauvignon')
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
