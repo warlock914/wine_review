97f921a0ed4312d39977131044ab5f6dbf4b6fb3
Alan_Garcia <warlock914@gmail.com>
2014-10-13 02:08:49 -0400

    Homework4_inclusion

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -3,4 +3,6 @@ class Wine < ActiveRecord::Base
 validates :year,
    numericality: { only_integer: true, greater_than_or_equal_to: 0 },
    unless: "year.blank?"
+   	validates :varietal, inclusion: { in: %w(MERLOT CHIANTI CABERNET),
+    message: "%{value} is not a valid type, must be MERLOT CHIANTI CABERNET" }
 end
